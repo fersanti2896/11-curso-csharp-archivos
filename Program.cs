f3a562cbcdcaeebe8c7988a44b77434cae43fdcf
Fernando Nicolás <fersanti2896@gmail.com>
2022-12-05 16:14:08 -0600

    Clase File en Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
 ﻿
 Console.WriteLine("¡TRABAJANDO CON ARCHIVOS!");
+
+var rutaOrigen = @"C:\Users\josantiago\Documents\Cursos Udemy\Programando en C# de Principiante a Profesional\S11 - Trabajando con Archivos\ejemplo.txt";
+var rutaDestino = @"C:\Users\josantiago\Documents\Cursos Udemy\Programando en C# de Principiante a Profesional\S11 - Trabajando con Archivos\ejemplo-copiado.txt";
+
+/* Clase File - Leyendo archivo */
+if (File.Exists(rutaOrigen)) {
+    var contenido = File.ReadAllText(rutaOrigen);
+    Console.WriteLine(contenido);
+    File.Copy(rutaOrigen, rutaDestino, overwrite: true);
+} else {
+    Console.WriteLine("El archivo no fue encontrado");
+}
+
+var contenidoLineas = File.ReadAllLines(@"C:\Users\josantiago\Documents\Cursos Udemy\Programando en C# de Principiante a Profesional\S11 - Trabajando con Archivos\ejemplo-lineas.txt");
+Console.WriteLine();
+
+foreach (var line in contenidoLineas) {
+    Console.WriteLine(line);
+}  
